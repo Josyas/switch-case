6a1e5d056331a504a1f56dcfb91f9ba109e50af2
Josias <87546094+Josyas@users.noreply.github.com>
2021-10-26 20:59:39 -0300

    Add files via upload

---
+++ switch case/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace switch_case {
+    class Program {
+        static void Main(string[] args) {
+            
+            int x = int.Parse(Console.ReadLine());
+            string day;
+
+            switch (x) {
+                case 1:
+                    day = "Segunda";
+                    break;
+                case 2:
+                    day = "Terça-Feira";
+                    break;
+                case 3:
+                    day = "Quarta-Feira";
+                    break;
+                case 4:
+                    day = "Quint-Feira";
+                    break;
+                case 5:
+                    day = "Sexta-Feira";
+                    break;
+                case 6:
+                    day = "Sábado";
+                    break;
+                case 7:
+                    day = "Domingo";
+                    break;
+                default:
+                    day = "Inavalido";
+                    break;
+            }
+            Console.WriteLine("Day: " + day);
+        }
+    }
+}
